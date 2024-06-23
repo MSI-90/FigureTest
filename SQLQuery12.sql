f5c04703e7e7dfa5de852a7a541a1c86c310ffb2
Сергей Миллер <104754352+MSI-90@users.noreply.github.com>
2024-06-23 16:22:37 +0700

    Update SQLQuery12.sql
    modify localisation for attributes captions of the result table

--- SQLQuery12.sql
+++ SQLQuery12.sql
@@ -1,4 +1,4 @@
-SELECT Products.product_name AS 'Наименование продукта', Category.category_name AS 'Наименование категории'
+SELECT Products.product_name AS 'Product_Name', Category.category_name AS 'Category_Name'
 FROM ProductCategory
 FULL OUTER JOIN Products ON ProductCategory.product_id = Products.id
 LEFT JOIN Category ON ProductCategory.category_id = Category.id;
